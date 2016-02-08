7e62e5e3468b7c582d7c4c6f86af9da9301df35c
Endel Dreyer <endel.dreyer@gmail.com>
2016-02-08 21:00:56 +0000

    bump v0.1.5

--- dist/websocket.js
+++ dist/websocket.js
@@ -1570,17 +1570,17 @@ function hasOwnProperty(obj, prop) {
 },{"./support/isBuffer":13,"_process":12,"inherits":8}],15:[function(require,module,exports){
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var backoff = require('backoff');
 
-var WebSocketClient = (function () {
+var WebSocketClient = function () {
 
   /**
    * @param url DOMString The URL to which to connect; this should be the URL to which the WebSocket server will respond.
@@ -1705,6 +1705,7 @@ var WebSocketClient = (function () {
 
   }, {
     key: 'close',
+
 
     /**
      * Closes the WebSocket connection or connection attempt, if any. If the
@@ -1864,7 +1865,7 @@ var WebSocketClient = (function () {
   }, {
     key: 'onreconnect',
     set: function set(listener) {
-      this.listener['onreconnect'] = listener;
+      this.listeners['onreconnect'] = listener;
     },
     get: function get() {
       return this.listeners['onreconnect'];
@@ -1872,11 +1873,12 @@ var WebSocketClient = (function () {
   }]);
 
   return WebSocketClient;
-})();
+}();
 
 /**
  * The connection is not yet open.
  */
+
 
 WebSocketClient.CONNECTING = WebSocket.CONNECTING;
 
